2df298778cbcafad51253c8c6f0d77c5bad00974
Bidisha Biswas <bdsh.14@gmail.com>
2023-04-23 13:10:45 -0600

    Initial Commit

---
+++ FunAnimations/FunAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunAnimationsApp.swift
+//  FunAnimations
+//
+//  Created by Bidisha Biswas on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct FunAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
