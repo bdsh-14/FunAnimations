680b7af3cfa66f138460686bf133a8c220e370f4
Bidisha Biswas <bdsh.14@gmail.com>
2023-05-01 19:19:43 -0600

    some more views

---
+++ FunAnimations/ProgressView.swift
@@ -0,0 +1,20 @@
+//
+//  ProgressView.swift
+//  FunAnimations
+//
+//  Created by Bidisha Biswas on 4/29/23.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressView()
+    }
+}
---
+++ FunAnimations/ScaleMovement.swift
@@ -0,0 +1,20 @@
+//
+//  ScaleMovement.swift
+//  FunAnimations
+//
+//  Created by Bidisha Biswas on 5/1/23.
+//
+
+import SwiftUI
+
+struct ScaleMovement: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScaleMovement_Previews: PreviewProvider {
+    static var previews: some View {
+        ScaleMovement()
+    }
+}
