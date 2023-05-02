1a6ac7370b6c593fcfcb02ad832ebd4bed66eb37
Bidisha Biswas <bdsh.14@gmail.com>
2023-05-01 19:20:11 -0600

    views

--- FunAnimations/PolygonsView.swift
+++ FunAnimations/PolygonsView.swift
@@ -1,20 +1,100 @@
-//
-//  PolygonsView.swift
-//  FunAnimations
-//
-//  Created by Bidisha Biswas on 4/29/23.
-//
-
 import SwiftUI
 
 struct PolygonsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@State private var sides: Double = 4
+	@State private var scale: Double = 1.0
+
+	var body: some View {
+		VStack {
+			PolygonShape(sides: sides, scale: scale)
+				.stroke(Color.purple, lineWidth: 5)
+				.frame(width: 350, height: 400)
+				.border(Color.black)
+				.padding()
+
+			Text("\(Int(sides)) sides, \(String(format: "%.2f", scale as Double)) scale")
+
+			HStack(spacing: 20) {
+				Button("2") {
+					withAnimation(Animation.easeInOut(duration: 1.0)) {
+						self.sides = 2.0
+						self.scale = 1.0
+					}
+				}
+
+				Button("3") {
+					withAnimation(Animation.easeInOut(duration: 1.0)) {
+						self.sides = 3.0
+						self.scale = 1.0
+					}
+				}
+
+				Button("7") {
+					withAnimation(Animation.easeInOut(duration: 1.0)) {
+						self.sides = 7.0
+						self.scale = 1.0
+					}
+				}
+
+				Button("10") {
+					withAnimation(Animation.easeInOut(duration: 1.0)) {
+						self.sides = 10.0
+						self.scale = 1.0
+					}
+				}
+
+			}
+		}
+		.navigationBarTitle("Polygons")
+	}
 }
 
+struct PolygonShape: Shape {
+	var sides: Double
+	var scale: Double
+
+	var animatableData: AnimatablePair<Double, Double> {
+		get { AnimatablePair(sides, scale) }
+		set {
+			sides = newValue.first
+			scale = newValue.second
+		}
+	}
+
+	func path(in rect: CGRect) -> Path {
+		// length hypotenuse
+		let hypotenuse = Double(min(rect.width, rect.height)) / 2.0 * scale
+
+		// center coordinates
+		let center = CGPoint(x: rect.width / 2.0, y: rect.height / 2.0)
+
+		var path = Path()
+
+		for side in 0..<Int(sides) {
+			let angle = (Double(side) * (360.0 / sides)) * (Double.pi / 180)
+
+			// Calculate vertex
+			let vertex = CGPoint(x: center.x + CGFloat(cos(angle) * hypotenuse),
+								 y: center.y + CGFloat(sin(angle) * hypotenuse))
+
+			if side == 0 {
+				path.move(to: vertex) // move to first vertex
+			} else {
+				path.addLine(to: vertex) // draw line to next vertex
+			}
+		}
+
+		// Drawing the polygon from a vertex instead of center so that we can close the path.
+
+		path.closeSubpath()
+
+		return path
+	}
+}
+
+// Previews
 struct PolygonsView_Previews: PreviewProvider {
-    static var previews: some View {
-        PolygonsView()
-    }
+	static var previews: some View {
+		PolygonsView()
+	}
 }
--- FunAnimations/ProgressView.swift
+++ FunAnimations/ProgressView.swift
@@ -1,16 +1,88 @@
-//
-//  ProgressView.swift
-//  FunAnimations
-//
-//  Created by Bidisha Biswas on 4/29/23.
-//
-
 import SwiftUI
 
 struct ProgressView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@State private var percent: CGFloat = 0
+
+	var body: some View {
+		VStack {
+			Spacer()
+			Color.clear.overlay(Indicator(pct: self.percent))
+
+			Spacer()
+			HStack(spacing: 10) {
+				Button {
+					withAnimation(.easeInOut(duration: 1.0)) { self.percent = 0 }
+				} label: {
+					Text("0%").font(.headline)
+				}
+
+				Button {
+					withAnimation(.easeInOut(duration: 1.0)) { self.percent = 0.27 }
+				} label: {
+					Text("27%").font(.headline)
+				}
+
+				Button {
+					withAnimation(.easeInOut(duration: 1.0)) { self.percent = 1.0 }
+				} label: {
+					Text("100%").font(.headline)
+				}
+			}
+		}.navigationBarTitle("Progress View")
+	}
+}
+
+struct Indicator: View {
+	var pct: CGFloat
+
+	var body: some View {
+		return Circle()
+			.fill(LinearGradient(gradient: Gradient(colors: [.blue, .pink, .green]), startPoint: .topLeading, endPoint: .bottomTrailing))
+			.frame(width: 150, height: 150)
+			.modifier(PercentageIndicator(pct: self.pct))
+	}
+}
+
+struct PercentageIndicator: AnimatableModifier {
+	var pct: CGFloat = 0
+
+	var animatableData: CGFloat {
+		get { pct }
+		set { pct = newValue }
+	}
+
+	func body(content: Content) -> some View {
+		content
+			.overlay(ArcShape(pct: pct).foregroundColor(.red))
+			.overlay(LabelView(pct: pct))
+	}
+
+	struct ArcShape: Shape {
+		let pct: CGFloat
+
+		func path(in rect: CGRect) -> Path {
+
+			var p = Path()
+
+			p.addArc(center: CGPoint(x: rect.width / 2.0, y:rect.height / 2.0),
+					 radius: rect.height / 2.0 + 5.0,
+					 startAngle: .degrees(0),
+					 endAngle: .degrees(360.0 * Double(pct)), clockwise: false)
+
+			return p.strokedPath(.init(lineWidth: 10, dash: [6, 3], dashPhase: 10))
+		}
+	}
+
+	struct LabelView: View {
+		let pct: CGFloat
+
+		var body: some View {
+			Text("\(Int(pct * 100)) %")
+				.font(.largeTitle)
+				.fontWeight(.bold)
+				.foregroundColor(.white)
+		}
+	}
 }
 
 struct ProgressView_Previews: PreviewProvider {
